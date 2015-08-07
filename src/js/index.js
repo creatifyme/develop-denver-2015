7449387d1b5800a514a9cf91e53364199aa7cc7b
talon.poole@gmail.com <talon.poole@gmail.com>
2015-08-07 16:03:04 -0600

    consume wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 /**
  * Dependencies
  */
-import speakerbox from './modules/speakerbox'
 import TweetStream from './TweetStream'
+import twitter from './twitter'
 
-TweetStream(twitterSocket)
-  // transform data...
-  .subscribe(/* render app */)
+TweetStream(twitter({
+  api: 'statuses/filter',
+  track: {track: 'dvlpdnvr'},
+  key: '4a82KzzoQqmbB9GoeXohQ',
+  secret: 'ZVYYIxWTH1ujEge0SliIPptFF0M4hRpi5Vb1fDjhCbY',
+  acc_key: '64105403-PIrbbf7ekv1uCqvgROMLJwngySSwmnom5oO66wJRn',
+  acc_secret: '0j59mARrd1ORgGTaprdRhGZf6rhhZrCf7tmJncg',
+}))
+  .subscribe((data => console.log(data)))
