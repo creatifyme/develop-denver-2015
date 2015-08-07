2a54cc0a300f60cc715fa2502a23a4a957b08e35
talon.poole@gmail.com <talon.poole@gmail.com>
2015-08-07 16:27:28 -0600

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,17 @@
 import TweetStream from './TweetStream'
 import twitter from './twitter'
 
+const words = string => string.split(' ')
+const tweetBody = tweet = tweet.body
+
 TweetStream(twitter({
   api: 'statuses/filter',
-  track: {track: 'dvlpdnvr'},
-  key: '4a82KzzoQqmbB9GoeXohQ',
-  secret: 'ZVYYIxWTH1ujEge0SliIPptFF0M4hRpi5Vb1fDjhCbY',
-  acc_key: '64105403-PIrbbf7ekv1uCqvgROMLJwngySSwmnom5oO66wJRn',
-  acc_secret: '0j59mARrd1ORgGTaprdRhGZf6rhhZrCf7tmJncg',
+  track: {track: 'dvlpdenver'},
+  key: 'HY1S6TbSlCdsUyeWs2N2FvX94',
+  secret: 'a8H8Xx6pSoJUyZP68lp6XTOSZY5mV38pWDj6lTk40ENZauSdeB',
+  acc_key: '64105403-amFRWE4lPkI7wJYki567wT03heqpAFX4LSnX4qert',
+  acc_secret: 'CUfVO9nOOG3dyxArp4ViuU1DFrQosNhL9ncLAtex6CmBp',
 }))
-  .subscribe((data => console.log(data)))
-  
+  .map(tweetBody)
+  .map(words)
+  .subscribe(data => console.log(data))
